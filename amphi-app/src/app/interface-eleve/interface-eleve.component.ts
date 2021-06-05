4b03fd33e55737467cfcc258fcbc108e99eb223a
adam <66301048+aadedjou@users.noreply.github.com>
2021-06-05 03:21:53 +0200

    Add component for student view

---
+++ amphi-app/src/app/interface-eleve/interface-eleve.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-interface-eleve',
+  templateUrl: './interface-eleve.component.html',
+  styleUrls: ['./interface-eleve.component.scss']
+})
+export class InterfaceEleveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
