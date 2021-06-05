2fb043483bb4bf7fd86b8a76bbe7ac0096b34c2d
adam <66301048+aadedjou@users.noreply.github.com>
2021-06-05 17:42:29 +0200

    Update student view component
    - Possibility to toggle 'focus mode'
    - Better displaying
    - Pop up messages

--- amphi-app/src/app/interface/interface-eleve/interface-eleve.component.ts
+++ amphi-app/src/app/interface/interface-eleve/interface-eleve.component.ts
@@ -1,13 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-interface-eleve',
   templateUrl: './interface-eleve.component.html',
   styleUrls: ['./interface-eleve.component.scss']
 })
-export class InterfaceEleveComponent implements OnInit {
-  constructor() { }
+export default class InterfaceEleveComponent implements OnInit {
+  focusMode : boolean = true;
+
+  constructor(private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+  }
+
+  toggleFocusMode() {
+    this.focusMode = !this.focusMode;
+  }
+
+  showFocusModeSnackBar() {
+    if (this.focusMode) return;
+
+    let snackBarRef = this._snackBar.open(
+      "Tu as quitté le mode 'Focus'.",
+      "Revenir au stream",
+      { duration: 4500 }
+    );
+
+    snackBarRef.onAction().subscribe(() => {
+      this.toggleFocusMode();
+    });
   }
 }
