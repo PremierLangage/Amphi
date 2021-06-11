762bec4ae5832b18b6471d91e09260eb3242b16a
adam <66301048+aadedjou@users.noreply.github.com>
2021-06-11 03:42:39 +0200

    Implement navigation and keyboard inputs for student view

--- amphi-app/src/app/interface/interface-eleve/interface-eleve.component.ts
+++ amphi-app/src/app/interface/interface-eleve/interface-eleve.component.ts
@@ -1,21 +1,73 @@
-import { Component, Input } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Component, Input, OnInit, HostListener } from '@angular/core';
 import { Presentation } from 'src/app/models/presentation';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { KeyCode } from '../interface.component';
 
 @Component({
   selector: 'app-interface-eleve',
   templateUrl: './interface-eleve.component.html',
   styleUrls: ['./interface-eleve.component.scss']
 })
-export default class InterfaceEleveComponent {
-  @Input() presentation !: Presentation;
+export class InterfaceEleveComponent implements OnInit {
+  @Input() globalPresentation !: Presentation;
+  ownPresentation !: Presentation;
 
   focusMode : boolean = true;
 
   constructor(private _snackBar: MatSnackBar) { }
 
+  ngOnInit() : void {
+    this.ownPresentation = new Presentation(
+      this.globalPresentation.title,
+      this.globalPresentation.slides
+    );
+    this.ownPresentation.currentSlide = -1;
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    switch (event.keyCode) {
+      case KeyCode.LEFT:
+        this.navigateBackwards();
+        break;
+      case KeyCode.RIGHT:
+        this.navigateForward();
+        break;
+      case KeyCode.FOCUS:
+        this.toggleFocusMode();
+        break;
+    }
+  }
+
   toggleFocusMode() {
     this.focusMode = !this.focusMode;
+    if (this.ownPresentation.currentSlide == -1)
+      this.ownPresentation.currentSlide = this.globalPresentation.currentSlide;
+    this.showFocusModeSnackBar();
+  }
+
+  navigateBackwards() {
+    if (this.ownPresentation.isOnFirstSlide()) return;
+    this.ownPresentation.setToPreviousSlide();
+  }
+
+  navigateForward() {
+    if (this.cantGoFurther()) return;
+    this.ownPresentation.setToNextSlide();
+  }
+
+  cantGoFurther() : boolean {
+    // returns true if the presentation is on last slide
+    // or if the next slide hasn't been revealed by the teacher yet
+    return this.globalPresentation.lastRevealedSlide <= this.ownPresentation.currentSlide || this.ownPresentation.isOnLastSlide()
+  }
+
+  currentSlide() : number {
+    return (this.focusMode ? this.globalPresentation : this.ownPresentation).currentSlide;
+  }
+
+  currentSlideContent() : string {
+    return (this.focusMode ? this.globalPresentation : this.ownPresentation).currentSlideContent();
   }
 
   showFocusModeSnackBar() {
