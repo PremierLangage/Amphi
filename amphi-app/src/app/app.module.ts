c071870e64934fc60444909b0e4c13a7b022431f
adam <66301048+aadedjou@users.noreply.github.com>
2021-06-15 15:01:58 +0200

    Add imports

--- amphi-app/src/app/app.module.ts
+++ amphi-app/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SlideComponent } from './interface/slide/slide.component';
+import { PlTextComponent } from './interface/pl-text/pl-text.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ExerciceComponent } from './interface/exercice/exercice.component';
 import { InterfaceComponent } from './interface/interface.component';
@@ -14,21 +16,22 @@ import { CurrentTimeComponent } from './interface/current-time/current-time.comp
 import { ProgressBarComponent } from './interface/progress-bar/progress-bar.component';
 import { InterfaceProfComponent } from './interface/interface-prof/interface-prof.component';
 import { InterfaceEleveComponent } from './interface/interface-eleve/interface-eleve.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // MATERIAL
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     SlideComponent,
+    PlTextComponent,
     ExerciceComponent,
     InterfaceComponent,
     PlatonLogoComponent,
@@ -44,6 +47,7 @@ import { FormsModule } from '@angular/forms';
     BrowserAnimationsModule,
     MatSlideToggleModule,
     MatSnackBarModule,
+    MatTooltipModule,
     AppRoutingModule,
     MatDialogModule,
     MatButtonModule,
