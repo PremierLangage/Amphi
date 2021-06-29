eff8e86bc999774162b3a777015da4f6653d186e
adam <66301048+aadedjou@users.noreply.github.com>
2021-06-29 15:05:08 +0200

    Fix progress-bar displaying problems

--- amphi-app/src/app/interface/progress-bar/progress-bar.component.ts
+++ amphi-app/src/app/interface/progress-bar/progress-bar.component.ts
@@ -6,8 +6,9 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./progress-bar.component.scss']
 })
 export class ProgressBarComponent {
+  @Input() forcedFocusMode : boolean = false;
+  @Input() focusMode : boolean = true;
   @Input() showFull : boolean = false;
-  @Input() focusMode : boolean = false;
   @Input() currentPage : number = 0;
   @Input() loadedPages : number = 0;
   @Input() max : number = 0;
